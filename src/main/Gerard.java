498d231a543c3d82b12696bb32e75d6d84de7e1c
GerardLaviana <gerardlaviana@gmail.com>
2022-11-04 11:53:58 +0100

    Clase Gerard modificada - Getter, Setters y Contructor creados

--- src/main/Gerard.java
+++ src/main/Gerard.java
@@ -5,4 +5,24 @@ public class Gerard {
 	private int altura;
 	private int peso;
 	
+	
+	public Gerard(int altura, int peso) {
+		this.altura = altura;
+		this.peso = peso;
+	}
+	
+	public int getAltura() {
+		return altura;
+	}
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	
+	
 }
