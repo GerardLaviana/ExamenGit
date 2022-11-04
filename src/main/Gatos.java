67a9cd5b8f78ec640bc967226d4d1735bc2f1dc6
GerardLaviana <gerardlaviana@gmail.com>
2022-11-04 11:50:57 +0100

    Clase Gatos modificada - Getter y Setters

--- src/main/Gatos.java
+++ src/main/Gatos.java
@@ -4,4 +4,25 @@ public class Gatos {
 
 	private String nombre;
 	private String raza;
+	private String genero;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getRaza() {
+		return raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
 }
